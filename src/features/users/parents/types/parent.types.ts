f4eac8d89720d7ccb4212b12d5d394811c56be1d
AishaKhairAllahOIO <aishakhairallah3@gmail.com>
2026-06-03 12:55:54 +0300

    edit parent user

--- src/features/users/parents/types/parent.types.ts
+++ src/features/users/parents/types/parent.types.ts
@@ -1,9 +1,13 @@
 import type { BaseUser } from "../../shared/types/base-user.types";
 
+export type ParentRelation = "father" | "mother" | "guardian" | "other";
+
 export type ParentUser = BaseUser & {
   category: "parent";
 
   parentCode: string;
+
+  relation: ParentRelation;
 
   occupation?: string | null;
 };
